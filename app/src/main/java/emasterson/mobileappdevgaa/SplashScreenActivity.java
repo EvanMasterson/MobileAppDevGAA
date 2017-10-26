6ae2fb4c09af978cfbbf427392c35d933c3419b7
EvanMasterson <evanm95@hotmail.com>
2017-10-26 10:38:45 +0100

    update launcher icon and add splash screen

---
+++ app/src/main/java/emasterson/mobileappdevgaa/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package emasterson.mobileappdevgaa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int seconds = 3;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, seconds * 1000);
+    }
+}
