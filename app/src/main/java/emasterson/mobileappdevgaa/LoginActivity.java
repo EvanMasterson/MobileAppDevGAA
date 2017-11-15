fe8a2e2e94679a6a0932381e91efbd9854386fd5
EvanMasterson <evanm95@hotmail.com>
2017-11-15 19:55:27 +0000

    switch to firebase twitter auth

--- app/src/main/java/emasterson/mobileappdevgaa/LoginActivity.java
+++ app/src/main/java/emasterson/mobileappdevgaa/LoginActivity.java
@@ -1,47 +1,122 @@
 package emasterson.mobileappdevgaa;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.TwitterAuthProvider;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthToken;
-import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+/*
+    @Reference
+    https://github.com/firebase/quickstart-android/tree/master/auth
+    https://firebase.google.com/docs/auth/android/twitter-login?authuser=0
+ */
 public class LoginActivity extends AppCompatActivity {
 
     private TwitterLoginButton loginButton;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Twitter.initialize(this);
         super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(getString(R.string.CONSUMER_KEY), getString(R.string.CONSUMER_SECRET));
+        TwitterConfig twitterConfig = new TwitterConfig.Builder(this).twitterAuthConfig(authConfig).build();
+        Twitter.initialize(twitterConfig);
+
         setContentView(R.layout.activity_login);
 
-        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        auth = FirebaseAuth.getInstance();
+        loginButton = findViewById(R.id.login_button);
+
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                Intent i = new Intent(LoginActivity.this, ClubListActivity.class);
-                startActivity(i);
-                finish();
+                handleTwitterSession(result.data);
             }
 
             @Override
             public void failure(TwitterException exception) {
-                Toast.makeText(LoginActivity.this, "Nosffsfoofjsaofjaofao", Toast.LENGTH_SHORT).show();
+                confirmUser(null);
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        confirmUser(currentUser);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleTwitterSession(TwitterSession session) {
+        showProgressDialog();
+
+        AuthCredential credential = TwitterAuthProvider.getCredential(session.getAuthToken().token, session.getAuthToken().secret);
+
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser currentUser = auth.getCurrentUser();
+                            confirmUser(currentUser);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            confirmUser(null);
+                        }
+                        hideProgressDialog();
+                    }
+                });
+    }
+
+    private void confirmUser(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            Intent i = new Intent(getApplicationContext(), ClubListActivity.class);
+            startActivity(i);
+        }
+    }
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
