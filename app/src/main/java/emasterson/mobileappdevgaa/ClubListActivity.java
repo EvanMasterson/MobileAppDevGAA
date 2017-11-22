313434c1f373acec536d6adbf2dae56acd6d2ae3
EvanMasterson <evanm95@hotmail.com>
2017-11-22 16:56:57 +0000

    logout options, added sound to splash screen, changed gif

--- app/src/main/java/emasterson/mobileappdevgaa/ClubListActivity.java
+++ app/src/main/java/emasterson/mobileappdevgaa/ClubListActivity.java
@@ -1,14 +1,43 @@
 package emasterson.mobileappdevgaa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ClubListActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_list);
 
+        auth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.logout:
+                auth.signOut();
+                Toast.makeText(getApplicationContext(), "Logged out", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/emasterson/mobileappdevgaa/SplashScreenActivity.java
+++ app/src/main/java/emasterson/mobileappdevgaa/SplashScreenActivity.java
@@ -2,24 +2,28 @@ package emasterson.mobileappdevgaa;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreenActivity extends Activity {
+    MediaPlayer media = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        int seconds = 8;
+        int seconds = 4;
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                media = MediaPlayer.create(getApplicationContext(), R.raw.loading_sound);
+                media.start();
                 finish();
             }
-        }, seconds * 1000);
+        }, seconds * 1125);
     }
 }
